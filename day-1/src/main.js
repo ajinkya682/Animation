4ffaf1bdf99ef0f9d2034ecbbf879fe7f3f234b8
Ajinkya Saivar <172878214+ajinkya682@users.noreply.github.com>
2026-06-09 16:44:54 +0530

    feat: implement interactive animations for all boxes in main.js

--- day-1/src/main.js
+++ day-1/src/main.js
@@ -1,43 +1,106 @@
 import "./style.css";
 import gsap from "gsap";
 
-gsap.to(".right", {
-  x: 800,
-  duration: 1.5,
+const boxes = {
+  right: document.querySelector(".right"),
+  left: document.querySelector(".left"),
+  top: document.querySelector(".top"),
+  bottom: document.querySelector(".bottom"),
+  scale: document.querySelector(".scale"),
+  rotate: document.querySelector(".rotate"),
+  fade: document.querySelector(".fade"),
+};
+
+let rightOpen = false;
+
+boxes.right.addEventListener("click", () => {
+  rightOpen = !rightOpen;
+
+  gsap.to(".right", {
+    x: rightOpen ? 800 : 0,
+    rotation: rightOpen ? 360 : 0,
+    scale: rightOpen ? 1.2 : 1,
+    duration: 1.5,
+    ease: "power3.inOut",
+  });
 });
 
-gsap.from(".left", {
-  x: 800,
-  duration: 1.5,
+boxes.left.addEventListener("click", () => {
+  const tl = gsap.timeline();
+
+  tl.from(".left", {
+    x: 800,
+    opacity: 0,
+    rotation: 180,
+    duration: 1.2,
+    ease: "back.out(1.7)",
+  }).to(".left", {
+    scale: 1.2,
+    duration: 0.3,
+    yoyo: true,
+    repeat: 1,
+  });
 });
 
-gsap.to(".top", {
-  y: -500,
-  duration: 1.5,
+boxes.top.addEventListener("click", () => {
+  gsap.fromTo(
+    ".top",
+    {
+      y: -500,
+      scale: 0.5,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      duration: 1.5,
+      ease: "bounce.out",
+    },
+  );
 });
 
-gsap.to(".bottom", {
-  y: 500,
-  duration: 1.5,
+boxes.bottom.addEventListener("click", () => {
+  gsap.to(".bottom", {
+    y: 500,
+    rotation: 720,
+    duration: 2,
+    ease: "elastic.out(1, 0.3)",
+  });
 });
 
-gsap.from(".scale", {
-  scale: 1.5,
-  duration: 1.5,
+boxes.scale.addEventListener("click", () => {
+  gsap.to(".scale", {
+    scale: 2,
+    rotation: 180,
+    duration: 1,
+    ease: "power2.out",
+    yoyo: true,
+    repeat: 1,
+  });
 });
 
-gsap.to(".rotate", {
-  rotation: 360,
-  duration: 1.5,
+boxes.rotate.addEventListener("click", () => {
+  gsap.to(".rotate", {
+    rotation: "+=360",
+    scale: 1.3,
+    duration: 1.2,
+    ease: "power4.out",
+    yoyo: true,
+    repeat: 1,
+  });
 });
 
-gsap.to(".fade", {
-  opacity: 0,
-  duration: 1,
-});
+let faded = false;
 
-gsap.from(".fade", {
-  delay: 1.5,
-  opacity: 1,
-  duration: 1,
+boxes.fade.addEventListener("click", () => {
+  faded = !faded;
+
+  gsap.to(".fade", {
+    opacity: faded ? 0 : 1,
+    scale: faded ? 0.5 : 1,
+    rotation: faded ? 180 : 0,
+    duration: 1,
+    ease: "power2.inOut",
+  });
 });
